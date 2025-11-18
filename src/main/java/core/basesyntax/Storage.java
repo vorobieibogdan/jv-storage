9c72944c7fa8ed1bffc5846deaee264cbd489495
Bohdan Vorobiei <vbs@ukr.net>
2025-11-18 16:53:08 +0200

    Tasks in progress2

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -1,28 +1,26 @@
 package core.basesyntax;
 
-import java.util.Arrays;
+public class Storage<K, V> {
 
-public interface Storage<K, V> implements Storage<K, V> {
     private static final int MAX_CAPACITY = 10;
 
-    private Object[] keys;
-    private Object[] values;
-
+    private final Object[] keys;
+    private final Object[] values;
     private int size;
 
-    public StorageImpl() {
-        this.keys = new Object[MAX_CAPACITY];
-        this.values = new Object[MAX_CAPACITY];
-        this.size = 0;
+    public Storage() {
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
+        size = 0;
     }
 
-    @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
             K currentKey = (K) keys[i];
 
-            if ((key == null && currentKey == null) || (key != null && key.equals(currentKey))) {
-                values[i] = value;
+            if ((key == null && currentKey == null)
+                    || (key != null && key.equals(currentKey))) {
+                values[i] = value; // replace existing value
                 return;
             }
         }
@@ -32,24 +30,21 @@ public interface Storage<K, V> implements Storage<K, V> {
             values[size] = value;
             size++;
         }
-
     }
 
     public V get(K key) {
         for (int i = 0; i < size; i++) {
             K currentKey = (K) keys[i];
 
-            if ((key == null && currentKey == null) || (key != null && key.equals(currentKey))) {
+            if ((key == null && currentKey == null)
+                    || (key != null && key.equals(currentKey))) {
                 return (V) values[i];
             }
         }
-
         return null;
     }
 
-    @Override
     public int size() {
         return size;
     }
 }
-
