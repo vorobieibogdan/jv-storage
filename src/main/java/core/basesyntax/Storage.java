2739b53113b0c1e2508fa3e74b3f25e07d168439
Bohdan Vorobiei <vbs@ukr.net>
2025-11-18 17:26:28 +0200

    Tasks in progress6

--- src/main/java/core/basesyntax/Storage.java
+++ src/main/java/core/basesyntax/Storage.java
@@ -6,47 +6,4 @@ public interface Storage<K, V> {
     V get(K key);
 
     int size();
-
-    class StorageImpl<K, V> implements Storage<K, V> {
-        private static final int MAX_CAPACITY = 10;
-
-        private final Object[] keys = new Object[MAX_CAPACITY];
-        private final Object[] values = new Object[MAX_CAPACITY];
-        private int size = 0;
-
-        @Override
-        public void put(K key, V value) {
-            for (int i = 0; i < size; i++) {
-                K currentKey = (K) keys[i];
-
-                if ((currentKey == null && key == null)
-                        || (currentKey != null && currentKey.equals(key))) {
-                    values[i] = value;
-                    return;
-                }
-            }
-
-            keys[size] = key;
-            values[size] = value;
-            size++;
-        }
-
-        @Override
-        public V get(K key) {
-            for (int i = 0; i < size; i++) {
-                K currentKey = (K) keys[i];
-
-                if ((currentKey == null && key == null)
-                        || (currentKey != null && currentKey.equals(key))) {
-                    return (V) values[i];
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public int size() {
-            return size;
-        }
-    }
 }
